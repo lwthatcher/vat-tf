aea07d00350c995fc8e7bc560406a8a5144b005b
Lawrence Thatcher <lwthatcher@msn.com>
2018-08-04 13:24:15 -0600

    added dataset

--- src/app/recognizer/recognizer.component.ts
+++ src/app/recognizer/recognizer.component.ts
@@ -35,10 +35,10 @@ export class RecognizerComponent implements OnInit {
     const ys = tf.tensor2d([1, 3, 5, 7], [4, 1]);
 
     // Train the model using the data.
-    model.fit(xs, ys, {epochs: 10}).then(() => {
+    model.fit(xs, ys, {epochs: 100}).then(() => {
       // Use the model to do inference on a data point the model hasn't seen before:
       // @ts-ignore
-      let result = model.predict(tf.tensor2d([5], [1, 1])) as Tensor;
+      let result = model.predict(tf.tensor2d([4, 5, 6, 7], [4, 1])) as Tensor;
       console.log('RESULT:', result.dataSync());
     });
   }
