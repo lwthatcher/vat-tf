f0c18ac19384b7acfb5ab239cda8714442e4e553
Lawrence Thatcher <lwthatcher@msn.com>
2018-08-04 15:21:55 -0600

    loaded data transformed into tensor

--- src/app/recognizer/recognizer.component.ts
+++ src/app/recognizer/recognizer.component.ts
@@ -3,6 +3,8 @@ import * as tf from '@tensorflow/tfjs';
 import * as d3 from "d3";
 import { Tensor } from '@tensorflow/tfjs';
 
+const DATA_LABELLED = {'pills-blue': true, 'pills-pink': true, 'run-red': false}
+
 
 @Component({
   selector: 'app-recognizer',
@@ -11,17 +13,25 @@ import { Tensor } from '@tensorflow/tfjs';
 })
 export class RecognizerComponent implements OnInit {
   // #region [Variables]
-  data;
+  data: Tensor;
+  labels: Tensor;
+  labelled: boolean;
+  canvas: HTMLCanvasElement;
+  context: CanvasRenderingContext2D;
   // #endregion
 
   // #region [Constructors]
   constructor() { }
 
-  async ngOnInit() {
-    let raw = await d3.text('../../assets/labelled.csv')
-    console.log('raw length:', raw.length);
-    this.data = this.parseCSV(raw);
-    console.log('parsed data', this);
+  ngOnInit() {
+    this.canvas = document.querySelector("canvas");
+    this.context = this.canvas.getContext("2d");
+
+    let dataset = 'pills-blue'
+
+    this.labelled = DATA_LABELLED[dataset]
+    this.loadData(dataset);
+    console.info('recognizer initialized', this);
   }
   // #endregion
 
@@ -30,7 +40,13 @@ export class RecognizerComponent implements OnInit {
   // #endregion
 
   // #region [Computation Methods]
-  compute_example() {
+  compute() {
+    console.log('do something!')
+  }
+  // #endregion
+
+  // #region [Examples]
+  toy_example() {
     // Define a model for linear regression.
     const model = tf.sequential();
     model.add(tf.layers.dense({units: 1, inputShape: [1]}));
@@ -53,9 +69,26 @@ export class RecognizerComponent implements OnInit {
   // #endregion
 
   // #region [Helper Methods]
+  private async loadData(dataset) {
+    let raw = await d3.text('../../assets/' + dataset + '.csv');
+    let data = this.parseCSV(raw);
+    let labels = []
+    if (this.labelled) {
+      data = data.map((row) => row.slice(0,-1));
+      labels = data.map((row) => row.slice(-1)[0]);
+    }
+    this.data = this.toTensor(data);
+    if (this.labelled) { this.labels = this.toTensor(labels) }
+    console.log('data loaded', this.data, this.labels);
+  }
+
   private parseCSV(data) {
     let asNumber = (d: string[]) => { return d.map((di) => +di) };
     return d3.csvParseRows(data, asNumber);
+  }
+
+  private toTensor(data) {
+    return tf.tensor(data);
   }
   // #endregion
 }
